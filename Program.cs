61435a75308671bbdff2044abb3018a4d96b9bf5
Shi Hao Wang <st890409@hotmail.com>
2017-09-06 14:57:03 +0800

    整數清單排序的範例
    進行整數清單排序：遞增，遞減。

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace net_core_sort_int_list
+{
+    class Program
+    {
+        static List<int> GetIntArray()
+        {
+            List<int> _items;
+
+            _items = new List<int>();
+            _items.Add(25);
+            _items.Add(5);
+            _items.Add(7);
+            _items.Add(18);
+            _items.Add(21);
+            _items.Add(14);
+            _items.Add(11);
+
+            return _items;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> _items;
+
+            _items = GetIntArray();
+
+            Console.WriteLine("orginal array");
+            Console.WriteLine(String.Join(",", _items.ToArray()));
+            Console.WriteLine();
+
+            _items = GetIntArray();
+            _items.Sort();
+
+            Console.WriteLine("sort with asc");
+            Console.WriteLine(String.Join(",", _items.ToArray()));
+            Console.WriteLine();
+
+            _items = GetIntArray();
+            _items.Sort();
+            _items.Reverse();
+
+            Console.WriteLine("sort with desc");
+            Console.WriteLine(String.Join(",", _items.ToArray()));
+            Console.WriteLine();
+        }
+    }
+}
